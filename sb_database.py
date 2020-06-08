e8273fd215d7a737560986577719796b7086e105
khushi02 <k9wadhwa@gmail.com>
2020-06-08 11:19:26 -0700

    Update sb_database.py

--- sb_database.py
+++ sb_database.py
@@ -2,34 +2,49 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# Declaring lists to store data
+cities = []
+sbs = []
 names = []
-amounts = []
+addresses = []
+images = []
 
+# Access city names and number of SBs
 url = 'https://supportlocal.usatoday.com/cities/'
 response = get(url)
-#print(response.text[:500])
 
 soup = BeautifulSoup(response.text, 'html.parser')
-#print(type(html_soup))
 
 city_containers = soup.find_all('a', class_ = 'sl-cities-list-item')
-#print(type(city_containers))
-print(len(city_containers))
-
-city_name = soup.find('a', class_ = 'sl-cities-list-item').h4.text
-print(city_name)
-
-num_sb = soup.find('div', class_ = 'sl-cities-list-item-count').span.text
-print(num_sb)
 
 for container in city_containers:
-    name = container.find('h4').text
-    names.append(name)
+    city = container.find('h4').text
+    cities.append(city)
     
     num = container.find('div', class_ = 'sl-cities-list-item-count').span.text
-    amounts.append(num)
+    sbs.append(num)
     
-test_df = pd.DataFrame({'City Name': names,
-                        'Number of SBs': amounts})
+city_df = pd.DataFrame({'City Name': cities,
+                        'Number of SBs': sbs})
 
-print(test_df)
+print(city_df)
+
+# Access individual city info
+for city in cities:
+    url_name = city.replace(' ', '-')
+    url_name = url_name.replace(',', '')
+    url_name = url_name.lower()
+    city_url = 'https://supportlocal.usatoday.com/city/' + url_name
+    city_response = get(city_url)
+    
+    city_soup = BeautifulSoup(city_response.text, 'html.parser')
+    sb_containers = city_soup.find_all('div', class_ = 'sl-business-list-item')
+    
+    for container in sb_containers:
+        name = container.find('div', class_= 'sl-business-list-item-details').p.a.text
+        names.append(name)
+        
+        address = container.find('p', class_ = 'address')
+        addresses.append(address)
+        
+        image = container.find('div', class_ = 'sl-business-list-item-img')
